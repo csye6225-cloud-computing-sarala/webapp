d5c2fe7ab82d0c1384c94f83b469ad0800be52b2
saralasharanappa <sharanappakanakagi.s@northeastern.edu>
2024-09-23 22:18:45 -0400

    fixed allowing unnecessary headers

--- src/services/database.js
+++ src/services/database.js
@@ -1,7 +1,27 @@
 import sequelize from "../config/database.js";
 
 async function handleHealthCheck(req, res) {
-  if (Object.keys(req.query).length > 0 || Object.keys(req.body).length > 0) {
+  // Define allowed headers
+  const allowedHeaders = [
+    "user-agent",
+    "accept",
+    "postman-token",
+    "host",
+    "accept-encoding",
+    "connection",
+  ];
+  console.log("Received headers:", req.headers);
+
+  // Check for any disallowed headers
+  const hasDisallowedHeaders = Object.keys(req.headers).some(
+    (header) => !allowedHeaders.includes(header.toLowerCase())
+  );
+
+  if (
+    Object.keys(req.query).length > 0 ||
+    Object.keys(req.body).length > 0 ||
+    hasDisallowedHeaders
+  ) {
     return res.status(400).end();
   }
   try {
