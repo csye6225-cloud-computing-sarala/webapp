677fcf41bb1e6941d614a5973c5b0c15ce9705fa
saralasharanappa <sharanappakanakagi.s@northeastern.edu>
2024-10-17 03:30:23 -0400

    edit postgres setup

--- packer/scripts/postgres_setup.sh
+++ packer/scripts/postgres_setup.sh
@@ -15,3 +15,26 @@ sudo apt-get install -y postgresql postgresql-contrib
 # Enable and start the PostgreSQL service
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
+
+# Set up PostgreSQL user and database
+if [ -z "${TEST_DB_USER}" ] || [ -z "${TEST_DB_PASSWORD}" ]; then
+  echo "DB_USER or DB_PASSWORD environment variable is not set. Exiting."
+  exit 1
+fi
+
+# Check if user exists, create or alter user if necessary
+if sudo -u postgres psql -tc "SELECT 1 FROM pg_roles WHERE rolname='${TEST_DB_USER}';" | grep -q 1; then
+  echo "User exists, altering password..."
+  sudo -u postgres psql -c "ALTER USER ${TEST_DB_USER} WITH PASSWORD '${TEST_DB_PASSWORD}';"
+else
+  echo "User does not exist, creating user..."
+  sudo -u postgres psql -c "CREATE USER ${TEST_DB_USER} WITH PASSWORD '${TEST_DB_PASSWORD}';"
+fi
+
+# Check if database exists, create database if necessary
+if sudo -u postgres psql -tc "SELECT 1 FROM pg_database WHERE datname='${TEST_DB_NAME}';" | grep -q 1; then
+  echo "Database ${TEST_DB_NAME} already exists."
+else
+  echo "Creating database ${TEST_DB_NAME}..."
+  sudo -u postgres psql -c "CREATE DATABASE ${TEST_DB_NAME} OWNER ${TEST_DB_USER};"
+fi
