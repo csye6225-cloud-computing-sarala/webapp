c8ac2e9d82f50e08b324773af0e5e5274658eb03
saralasharanappa <sharanappakanakagi.s@northeastern.edu>
2024-10-17 02:52:34 -0400

    changed ev

--- packer/scripts/node_setup.sh
+++ packer/scripts/node_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Install Node.js and npm
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
