d067ccd799030f10030e0ba62b25ea8bee9a53f3
saralasharanappa <sharanappakanakagi.s@northeastern.edu>
2024-10-30 22:50:38 -0400

    removed credentials

--- src/config/statsd.js
+++ src/config/statsd.js
@@ -14,10 +14,6 @@ const statsdClient = new StatsD({
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION || "us-east-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 });
 
 // Middleware to track API calls and duration
